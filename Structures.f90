cbf677c5eb9d35d453314afc7ab3d731e7f0aae6
Gorden Wunderlich <Gorden.Wunderlich@uni-duesseldorf.de>
2017-03-23 15:41:22 +0100

    made get_data more verbose about what it is doing

--- Structures.f90
+++ Structures.f90
@@ -4,47 +4,123 @@ use LinkedList
 contains
     pure module subroutine get_data(dest,source)
         class(*), dimension(:), intent(out) :: dest
-        class(*), dimension(:), allocatable, intent(in) :: source
-        select type(s=>source)
-        class default
-            select type(d => dest)
-                type is (integer)
-                    d=transfer(s,d)
+        class(*), dimension(:), intent(in) :: source
+        if(same_type_as(dest,source)) then
+            select type(s => source)
+            type is (integer)
+                select type(d => dest)
+                    type is (integer)
+                        d = s
+                    endselect
                 type is (real)
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (real)
+                        d = s
+                    endselect
                 type is (character(*))
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (character(*))
+                        d = s
+                    endselect
                 type is (complex)
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (complex)
+                        d = s
+                    endselect
                 type is (logical)
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (logical)
+                        d = s 
+                    endselect
                 type is (list)
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (list)
+                        d = s
+                    endselect
                 !add custom type selects here
             endselect
-        endselect
+        endif
     endsubroutine
     
     pure module subroutine get_data_single(dest,source)
         class(*), intent(out) :: dest
-        class(*), dimension(:), allocatable, intent(in) :: source
-        select type(s=>source(1))
-        class default
-            select type(d => dest)
-                type is (integer)
-                    d=transfer(s,d)
+        class(*), dimension(:), intent(in) :: source
+        if(same_type_as(dest,source)) then
+            select type(s => source(1))
+            type is (integer)
+                select type(d => dest)
+                    type is (integer)
+                        d = s
+                    endselect
                 type is (real)
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (real)
+                        d = s
+                    endselect
                 type is (character(*))
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (character(*))
+                        d = s
+                    endselect
                 type is (complex)
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (complex)
+                        d = s
+                    endselect
                 type is (logical)
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (logical)
+                        d = s 
+                    endselect
                 type is (list)
-                    d=transfer(s,d)
+                    select type(d => dest)
+                    type is (list)
+                        d = s
+                    endselect
                 !add custom type selects here
             endselect
-        endselect
+        endif
     endsubroutine
+    
+    pure module function compare_data(data1,data2) result(res)
+        class(*), dimension(:), intent(in) :: data1, data2
+        logical :: res
+        res = .false.
+        if(same_type_as(data1,data2)) then
+            select type(d1=>data1)
+            type is (integer)
+                select type(d2 => data2)
+                    type is (integer)
+                        res = all(d1 .eq. d2) 
+                    endselect
+                type is (real)
+                    select type(d2 => data2)
+                    type is (real)
+                        res = all(d1 .eq. d2) 
+                    endselect
+                type is (character(*))
+                    select type(d2 => data2)
+                    type is (character(*))
+                        res = all(d1 .eq. d2) 
+                    endselect
+                type is (complex)
+                    select type(d2 => data2)
+                    type is (complex)
+                        res = all(d1 .eq. d2) 
+                    endselect
+                type is (logical)
+                    select type(d2 => data2)
+                    type is (logical)
+                        res = all(d1 .eqv. d2) 
+                    endselect
+                type is (list)
+                    select type(d2 => data2)
+                    type is (list)
+                        !res = all(d1 .eq. d2) 
+                    endselect
+                !add custom type selects here
+            endselect
+        endif
+    endfunction
+    
 endsubmodule    
--- Structures_Interface.f90
+++ Structures_Interface.f90
@@ -2,13 +2,18 @@ module struct_util
     interface
         pure module subroutine get_data(dest,source)
             class(*), dimension(:), intent(out) :: dest
-            class(*), dimension(:), allocatable, intent(in) :: source
+            class(*), dimension(:), intent(in) :: source
         endsubroutine
     
         pure module subroutine get_data_single(dest,source)
             class(*), intent(out) :: dest
-            class(*), dimension(:), allocatable, intent(in) :: source
+            class(*), dimension(:), intent(in) :: source
         endsubroutine
+
+        pure module function compare_data(data1,data2) result(res)
+            class(*), dimension(:), intent(in) :: data1, data2
+            logical :: res
+        endfunction
     
     endinterface
 endmodule
